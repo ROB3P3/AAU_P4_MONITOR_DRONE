35f10dde39a20054234d06f5b8e780d618493db7
frederik <frederik@webupdater.dk>
2024-04-19 09:41:38 +0200

    WeGo

--- ros2_ws/build/my_robot_controller/prefix_override/sitecustomize.py
+++ ros2_ws/build/my_robot_controller/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/drone/P4Code/AAU_P4_MONITOR_DRONE/ros2_ws/install/my_robot_controller'
+sys.prefix = sys.exec_prefix = '/home/frederik/Documents/P4/AAU_P4_MONITOR_DRONE/ros2_ws/install/my_robot_controller'
