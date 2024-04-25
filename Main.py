866a181cc4e17ee0c5c1c76d6ed7cf11371bfb3d
RasmusBJensen <117160777+RasmusBJensen@users.noreply.github.com>
2024-04-25 12:04:59 +0200

    nye navne i main

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import LOGIK.Path_generator_v2_vp_4_Final as pg
 import numpy as np
 
 if __name__ == "__main__":
-    positions = path_generator()
-    #print('Positions:', positions)
-    positions =  pg.length_of_trajectory(positions)
+    scan_pattern = path_generator()
+    #print('scan_pattern:', scan_pattern)
+    positions =  pg.length_of_trajectory(scan_pattern)
     positions = pg.velocity(positions)
     #print('Positions:', positions)
     All_polynomials = pg.Cubic_polynomial_trajectory_vp(positions)
