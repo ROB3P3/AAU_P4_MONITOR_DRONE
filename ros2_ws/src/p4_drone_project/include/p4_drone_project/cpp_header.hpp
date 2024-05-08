1513d829114ca40be555f4081e663a5a661c8770
takenakabayashi <takekazunakabayashi@gmail.com>
2024-05-08 15:56:45 +0200

    readded code that disappeared

--- ros2_ws/src/p4_drone_project/include/p4_drone_project/cpp_header.hpp
+++ ros2_ws/src/p4_drone_project/include/p4_drone_project/cpp_header.hpp
@@ -37,7 +37,6 @@ class ViconClient : public rclcpp::Node
                 // if connection get info about drone and publish it to the "pid_regulator_vicon" topic
                 ViconDataStreamSDK::CPP::Output_UpdateFrame frame = ViconRetimingClient.UpdateFrame();
                 ViconDataStreamSDK::CPP::Output_GetSubjectCount viconSubjectCount = ViconRetimingClient.GetSubjectCount();
-                RCLCPP_INFO(this->get_logger(), "Number of subjects: %d", viconSubjectCount.SubjectCount);
                 if(viconSubjectCount.Result == ViconDataStreamSDK::CPP::Result::Success)
                 {
                     for (unsigned int SubjectIndex = 0; SubjectIndex < viconSubjectCount.SubjectCount; ++SubjectIndex)
@@ -45,23 +44,18 @@ class ViconClient : public rclcpp::Node
                         ViconDataStreamSDK::CPP::Output_GetSubjectName subjectName = ViconRetimingClient.GetSubjectName(SubjectIndex);
                         if(subjectName.Result == ViconDataStreamSDK::CPP::Result::Success)
                         {
-                            RCLCPP_INFO(this->get_logger(), "Subject %d: %s", SubjectIndex, subjectName.SubjectName);
                             ViconDataStreamSDK::CPP::Output_GetSegmentCount segmentCount = ViconRetimingClient.GetSegmentCount(subjectName.SubjectName);
                             if(segmentCount.Result == ViconDataStreamSDK::CPP::Result::Success)
                             {
-                                RCLCPP_INFO(this->get_logger(), "Segment count: %d", segmentCount.SegmentCount);
                                 for (unsigned int SegmentIndex = 0; SegmentIndex < segmentCount.SegmentCount; ++SegmentIndex)
                                 {
                                     ViconDataStreamSDK::CPP::Output_GetSegmentName segmentName = ViconRetimingClient.GetSegmentName(subjectName.SubjectName, SegmentIndex);
                                     if(segmentName.Result == ViconDataStreamSDK::CPP::Result::Success)
                                     {
-                                        RCLCPP_INFO(this->get_logger(), "Segment %d: %s", SegmentIndex, segmentName.SegmentName);
                                         ViconDataStreamSDK::CPP::Output_GetSegmentGlobalTranslation globalTranslation = ViconRetimingClient.GetSegmentGlobalTranslation(subjectName.SubjectName, segmentName.SegmentName);
                                         ViconDataStreamSDK::CPP::Output_GetSegmentLocalRotationEulerXYZ globalRotation = ViconRetimingClient.GetSegmentLocalRotationEulerXYZ(subjectName.SubjectName, segmentName.SegmentName);
                                         if(globalTranslation.Result == ViconDataStreamSDK::CPP::Result::Success && globalRotation.Result == ViconDataStreamSDK::CPP::Result::Success)
                                         {
-                                            RCLCPP_INFO(this->get_logger(), "Global Translation: %f %f %f", globalTranslation.Translation[0], globalTranslation.Translation[1], globalTranslation.Translation[2]);
-                                            RCLCPP_INFO(this->get_logger(), "Global Rotation: %f %f %f", globalRotation.Rotation[0], globalRotation.Rotation[1], globalRotation.Rotation[2]);
                                             auto message = std_msgs::msg::Float64MultiArray();
                                             message.data = {globalTranslation.Translation[0], globalTranslation.Translation[1], globalTranslation.Translation[2], globalRotation.Rotation[2]};
                                             RCLCPP_INFO(this->get_logger(), "Publishing: '[%f, %f, %f]'", message.data[0], message.data[1], message.data[2], message.data[3]);
--- ros2_ws/src/p4_drone_project/p4_drone_project/motion_controller_module.py
+++ ros2_ws/src/p4_drone_project/p4_drone_project/motion_controller_module.py
@@ -29,7 +29,7 @@ URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
 DEFAULT_HEIGHT = 1.0
 BASE_VELOCITY = 0.085
 REGULATOR_VALUE = 2000.0
-SLOWDOWN_VELOCITY_MAX = 0.015
+VELOCITY_RANGE = 0.015
 ERROR_RANGE = 5.0
 POLY_DELTA_T = 0.5
 deck_attached_event = Event()
@@ -88,7 +88,6 @@ class MotionControllerNode(Node):
         fly = True
         points = [[0, 0, 100.0, 0.0, 5.0], [-50.0, 50.0, 100.0, 5.0, 8.54], [-150.0, 50.0, 100.0, 8.54, 13.54], [-150.0, -150.0, 100.0, 13.54, 18.54], [-50.0, -150.0, 100.0, 18.54, 23.54], [0, 0, 100.0, 23.54, 28.54]]
 
-
         #positions = [(0, 0, 100.0, 10, 10, 0, 122.96232256791887, 10.0), (63.99009900990099, 105.0, 100.0, 10, 10, 0, 86.69139470353151, 22.29623225679189)]
         #polynomials = [[sympy.parse_expr('10*t + 63.4397299691071*(0.1*t - 1)**3 - 117.010448101245*(0.1*t - 1)**2 - 100.0'), sympy.parse_expr('10*t + 19.3230782998319*(0.1*t - 1)**3 - 35.6401588036363*(0.1*t - 1)**2 - 100.0'), np.poly1d([100.]), 12.296232256791889, 10.0, 22.29623225679189]]
 
@@ -123,4 +122,79 @@ class MotionControllerNode(Node):
 
                 rclpy.spin_once(self.velocityNode)
                 self.prevViconPoint = viconPoint
-                viconPoint 
+                viconPoint = self.velocityNode.viconPoint
+                error = [points[flyingTo][0] - viconPoint[0], points[flyingTo][1] - viconPoint[1], points[flyingTo][2] - viconPoint[2]]
+
+                # regulate on the forhold of speed impact of regulator and base speed
+                # change so the velocity close to points does not change so binaryily
+
+                #velocity = [max(min(0.2, error[0] / 50.0), -0.2), max(min(0.2, error[1] / 50.0), -0.2), 0.0, 0.0]
+                #velocity = [max(min(VELOCITY_MAX, error[0] / REGULATOR_VALUE), -VELOCITY_MAX), max(min(VELOCITY_MAX, error[1] / REGULATOR_VALUE), -VELOCITY_MAX), 0.0, 0.0]
+
+                velocityX = -BASE_VELOCITY if error[0] < 0 else BASE_VELOCITY
+                velocityY = -BASE_VELOCITY if error[1] < 0 else BASE_VELOCITY
+
+                velocityX += max(min(0, error[0] / REGULATOR_VALUE), -VELOCITY_RANGE) if error[0] < 0 else min(VELOCITY_RANGE, error[0] / REGULATOR_VALUE)
+                velocityY += max(min(0, error[1] / REGULATOR_VALUE), -VELOCITY_RANGE) if error[1] < 0 else min(VELOCITY_RANGE, error[1] / REGULATOR_VALUE)
+
+                if error[0] < ERROR_RANGE and error[0] > -ERROR_RANGE and error[1] < ERROR_RANGE and error[1] > -ERROR_RANGE:
+                    flyingTo += 1
+                    if flyingTo == len(points):
+                        self.get_logger().info("flight ended")
+                        fly = False
+                    else:
+                        self.get_logger().info("---------------------------------------------------- flying to next point: " + str(points[flyingTo]) + " ---------------------------------------------------------------------------")
+
+                velocity = [velocityX, velocityY, 0.0, 0.0]
+
+                self.get_logger().info("Error: " + str(error))
+                self.get_logger().info("Velocity: " + str(velocity))
+                mc.start_linear_motion(velocity[0], velocity[1], velocity[2], velocity[3])
+                
+                if keyInput == 'q':
+                    fly = False
+            self.get_logger().info("Flight ended!")
+
+
+    def getKey(self):
+        settings = termios.tcgetattr(sys.stdin)
+        timeout = 0.1
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select([sys.stdin], [], [], timeout)
+        if rlist:
+            key = sys.stdin.read(1)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
+
+# motion commander node step-by-step :
+# first initiate connection to crazyflie + crazyradio
+# then initiate subscriber connection to regulator
+# when receiving information from regulator start flying
+# use the command start_linear_motion to start flying
+# run the flying command in a while loop? where the condition is that it has not yet reached the last point?
+# if the start_linear_motion command runs in a while loop a separate node is needed to subscribe to the regulator
+# having a separate node and spinning this node can take some time so the publisher timer of the regulator needs to be significantly lower
+# when out of the while loop finish the 'with' context and land
+
+class VelocityRecipientNode(Node):
+    def __init__(self):
+        super().__init__("velocity_recipient_node")
+        #self.regulatorSubscriber_ = self.create_subscription(RegulatedVelocity, "/motioncontroller_regulator", self.onRegulatorMsg, 10)
+        self.viconSubscriber_ = self.create_subscription(Float64MultiArray, "/pid_regulator_vicon", self.onViconMsg, 10)
+        self.receivedVelocity = []
+        self.flightStatus = True
+        self.viconPoint = []
+    
+    def onRegulatorMsg(self, msg):
+        """Callback function for subscription to '/motioncontroller_regulator' topic"""
+        self.get_logger().info("Velocity Recipient Node received message from regulator topic.")
+        self.get_logger().info("Received message: " + str(msg.data))
+        self.get_logger().info("Received status: " + str(msg.status))
+        self.receivedVelocity = msg.data
+        self.flightStatus = msg.status
+    
+    def onViconMsg(self, msg):
+        self.viconPoint = [msg.data[0] / 10.0, msg.data[1] / 10.0, msg.data[2] / 10.0, math.degrees(msg.data[3])]
+        self.get_logger().info("Received position from VICON: " + str(self.viconPoint))
